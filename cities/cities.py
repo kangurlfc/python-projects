0af0274e0f96b49fbff1576be594be0e1708dc82
Martin Ismail <martinismail6@gmail.com>
2024-11-08 20:42:02 +0100

    added error checks, more functions, a way to print a city and append others to a tabulate table

--- cities/cities.py
+++ cities/cities.py
@@ -1,16 +1,26 @@
 import re
 import requests
-import tabulate
+from tabulate import tabulate
 
 
 class City:
-    def __init__(self) -> None:
-        self.inp = input("City: ").strip().lower().capitalize().replace(' ', '_')
-        # maybe i should let the user pass in an argument as well
+    def __init__(self, city: str = None) -> None:
+        self.inp = (city or input("City: ")).strip().lower().capitalize().replace(' ', '_')
+        if not self.inp.replace('_', '').isalpha():
+            raise ValueError("Enter a valid city")
 
     def __str__(self):
-        # this would print every attribute
-        ...
+        table = {
+            "City": self.inp.replace('_', ' ').title(),
+            "Country": self.country,
+            "Latitude": self.latitude,
+            "Longitude": self.longitude,
+            "Population": self.population,
+            "Local date & time": self.datetime,
+            "Temperature": self.temperature,
+            "Windspeed": self.windspeed
+        }
+        return table
 
     @property
     def get_wiki(self) -> dict:
@@ -26,19 +36,24 @@ class City:
         if matches := re.search(
                 r"\{\{coord\|((?:\d{1,2}\|){3}[NS])\|(\d{1,3}\|(?:\d{1,2}\|){2}[EW])", self.get_wiki["source"], re.I):
             return matches.group(1, 2)
+        else:
+            raise ValueError("Invalid city or coordinates unavailable")
 
     @property
     def population(self) -> str:
         if matches := re.search(
-                r"\|\s*population(?:(?:_blank1)|(?:_total)|(?:_urban)?)\s*=\s*(?:{{\w+}} )?([\d, ]+)", self.get_wiki["source"], re.I):
-            # kraków? munich glasgow perth
+                r"\|\s*population(?:_blank1|_total|_urban)?\s*=\s*(?:{{\w+}} )?([\d,]+(?:\s?[\d,]+)*)",
+                self.get_wiki["source"], re.I):
             return matches.group(1)
+        else:
+            return "n/a"
 
     @property
     def input_coord(self) -> tuple:
         latitude = self.coordinates[0].replace('|', '.', 1).split('|')[0]
         longitude = self.coordinates[1].replace('|', '.', 1).split('|')[0]
-        return latitude, longitude
+        return (latitude if 'N' in self.coordinates[0] else -float(latitude),
+                longitude if 'E' in self.coordinates[1] else -float(longitude))
 
     @property
     def latitude(self) -> str:
@@ -58,6 +73,8 @@ class City:
             for country in countries:
                 if country.strip() in matches.group(1):
                     return country.strip()
+        else:
+            raise ValueError("Enter a valid city")
 
     @property
     def temperature(self) -> str:
@@ -83,9 +100,23 @@ class City:
 
 
 def main():
+    all_data = []
+    add_more = True
+    while add_more:
+        city = City()
+        city_data = city.__str__()
+        all_data.append(city_data)
+        print(tabulate(all_data, headers="keys", tablefmt="grid", showindex='always'))
+        add_more = another_city()
 
-    city = City()
-    print(city.population)
+
+def another_city():
+    while True:
+        q1 = input("Would you like to add another city? ").lower().strip()
+        if q1 in ['y', 'yes']:
+            return True
+        elif q1 in ['n', 'no']:
+            return False
 
 
 if __name__ == "__main__":
---
+++ cities/readme.md
@@ -0,0 +1,5 @@
+
+
+you can enter a city two ways
+
+this program takes use only of free and keyless APIs therefore the data might be quite limited
